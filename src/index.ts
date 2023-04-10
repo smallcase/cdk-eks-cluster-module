b4bd0cc8bb29be6a822b7936697944732bb5739b
Bharat Parmar <bharat.parmar@smallcase.com>
2023-04-10 17:04:36 +0530

    feat: core addon permission (#29)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './constructs/eks-managed-addon';
+export * from './constructs/core-addon';
 export * from './constructs/eks-cluster';
 export * from './constructs/common-helm-charts';
